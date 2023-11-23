6c83e7cd71f5411db73656eb4ebe7b6e01fc71a6
강현철 <khc97622@naver.com>
2023-11-24 02:10:41 +0900

    캡스톤1

---
+++ OCR.py
@@ -0,0 +1,72 @@
+from PIL import Image
+import pytesseract
+
+
+
+class OCRClASS: #클래스화 다른 클래스에서 객체화 하려면 이과정이 필요합니다!
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+    imgPath1=""
+    text=None
+    #img = Image.open('img.jpg')
+    def imageNorm(self):
+        img= Image.open(self.imgPath1)
+        img = img.resize((900, 900))
+        self.text = pytesseract.image_to_string(img, lang='kor')
+        print("---OCR를 사용하여 이미지의 내용 처리----")
+        print(self.text)
+        print("--------------------------------------------------")
+
+
+
+    def init(self,imgPath):
+        self.imgPath1=imgPath
+
+    def text_process(self,input):
+        list = []
+        asicc = [chr(num) for num in range(33, 127)]
+        asicc.append("\n")
+        for i in range(0, len(input)):
+            for t in range(0, len(asicc)):
+                if input[i] == asicc[t] or (input[i] == "원" and input[i - 1] == asicc[t]):
+                    list.append(" ")
+                    list.append(" ")
+                    break
+                elif t == len(asicc) - 1 and asicc[t] != input[i]:
+                    list.append(input[i])
+                else:
+                    continue
+        return list
+
+
+    def out_process(self,out): #out 은 list
+
+        output = []
+        word = ''
+        i = 0
+        while i < len(out):
+            if out[i] != ' ':
+                word += out[i]
+            elif i < (len(out) - 2) and out[i] == ' ' and out[i - 1] != ' ':
+                if out[i + 1] != ' ':
+                    word += out[i]
+                    i += 1
+                    word += out[i]
+
+                elif out[i + 1] == ' ' and word != '':
+                    output.append(word)
+                    word = ""
+            i += 1
+        return output
+
+    def getter(self):
+        print(self.imgPath1)
+        self.imageNorm()
+        l = self.text_process(self.text)#이게 문제
+        print("debuging",l)
+        output = self.out_process(l)
+
+        print('-----츨력 데스트 처리-----')
+        for i in range(0, len(output)):
+            print(output[i])
+        return output
--- socket_server.py
+++ socket_server.py
@@ -2,8 +2,10 @@
 
 import socketserver
 import threading
-import connector_predict
+
+import OCR
 import random
+from OCR import OCRClASS
 
 
 class RequestHandler(socketserver.StreamRequestHandler):
@@ -12,13 +14,21 @@ class RequestHandler(socketserver.StreamRequestHandler):
         # get socket request
         socket = self.request
 
+        ocrStringArray= []
+        ocrString=""
+
         # show client
         print('Connect with : ' + self.client_address[0])
 
         # set file name
         num = random.random() * 100000
         file_name = 'image_temp/file_' + str(int(num)) + '.png'
-        print(file_name)
+        file_name1='C:/OCR/img.jpg'
+        print("1:"+file_name)
+        ocr1=OCR.OCRClASS()
+        ocr1.init(file_name)
+
+
 
         # get image file size from client
         file_size = socket.recv(1024) #이게 문제인듯?
@@ -42,16 +52,23 @@ class RequestHandler(socketserver.StreamRequestHandler):
                     break
 
         print('received & save image : ' + file_name)
+        ocrStringArray=ocr1.getter()
+
+        for i in range(0, len(ocrStringArray)):
+            ocrString+=ocrStringArray[i]+" "
+        print("stringOcr "+ocrString+" ")
+
 
         # tensorflow image classfication
-        connector_inst = connector_predict.Connect(file_name)
-        label = connector_inst.get_result()
-        socket.sendall(label)
+        #connector_inst = connector_predict.Connect(file_name)
+        #label = connector_inst.get_result()
+        #label=
+        socket.sendall(ocrString.encode())
         socket.close()
 
         
 if __name__ == '__main__':
-    HOST = '192.168.0.14'
+    HOST = '13.209.22.163'
     PORT = 8000
 
     server = socketserver.TCPServer((HOST, PORT), RequestHandler)
